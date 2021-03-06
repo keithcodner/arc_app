ec9ffb30bb718c6b2fca485ecf3430d8fdcb0e8e
kj blue <kjbluebombermegaman@hotmail.com>
2021-03-06 17:39:23 -0500

    file name corrections

--- server/controllers/arc_c_user..controller.js
+++
@@ -1,7 +0,0 @@
-const UserModel = require('../models/arc_c_user.model');
-const HttpException = require('../utils/HttpException.utils');
-const { validationResult } = require('express-validator');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
-dotenv.config();
