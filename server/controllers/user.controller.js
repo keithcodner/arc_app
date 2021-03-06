c5af45687d07237d0777b209eafd4095bd4e416b
kj blue <kjbluebombermegaman@hotmail.com>
2021-03-06 16:46:48 -0500

    added models, controllers, and routes files

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,7 @@
+const UserModel = require('../models/user.model');
+const HttpException = require('../utils/HttpException.utils');
+const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
---
+++ server/routes/user.route.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/user.controller');
+const auth = require('../middleware/auth.middleware');
+const Role = require('../utils/userRoles.utils');
+const awaitHandlerFactory = require('../middleware/awaitHandlerFactory.middleware');
